fdbd8ade0ee2d2bac6519d38c4dcfa5d22e63156
bertin.dev <bertmoun@yahoo.fr>
2021-08-17 14:52:12 +0100

    init SplashScreen

---
+++ app/src/main/java/com/oz_stream/tv/main/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.oz_stream.tv.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.oz_stream.tv.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
