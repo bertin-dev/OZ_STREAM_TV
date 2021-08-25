2188056e36d0ff32a3b1965c4b31e68be1b676a5
oz-stream <ozstreamtv@gmail.com>
2021-08-25 15:16:56 +0100

    Activity de base dont hériterons d'autres activity

---
+++ app/src/main/java/com/oz_stream/tv/ui/base/BaseTVActivity.java
@@ -0,0 +1,44 @@
+package com.oz_stream.tv.ui.base;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.os.Bundle;
+
+import java.util.Locale;
+
+
+public class BaseTVActivity extends Activity {
+
+    String currentLanguage = (Locale.getDefault().getLanguage().contentEquals("fr")) ? "fr" : "en", currentLang;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //langue par défaut
+        currentLanguage = getIntent().getStringExtra(currentLang);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+    }
+
+    public void addFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.tv_frame_content, fragment);
+        fragmentTransaction.commit();
+    }
+
+    /**
+     * attachBaseContext(Context newBase) methode callback permet de verifier la langue au demarrage
+     * @param newBase
+     * @since 2021
+     */
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(LocaleHelper.onAttach(newBase));
+    }
+}
